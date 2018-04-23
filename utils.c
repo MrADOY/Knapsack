444f1b67ceb384513a6e17097a253d4edfd9be35
Aurelien Pietrzak <pietrzak.aurelien@gmail.com>
2018-04-23 19:53:57 +0200

    Fix combinaison function

--- utils.c
+++ utils.c
@@ -1,37 +1,52 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
-void print_tab(int* tab,int size){
-  for (int i = 0;i < size; i++){
-    printf("%d ", tab[i]);
+/*
+* Print a int** tab
+*/
+
+void print_tab(int** tab, int n){
+  int j = 0;
+  for (int i = 0; i < (1 << n) - 1; i++) {
+    while(tab[i][j] != -1){
+      printf("%d ", tab[i][j]);
+      j++;
+    }
+    j = 0;
+    printf("\n");
   }
-  printf("\n");
 }
 
-int* generate_combinaisons(const int size){
+/*
+* Generate all the combinaison of n elements
+*/
 
-  /*
-  * Generate tab which contains values 1 to n
-  */
-  int* tab = NULL;
-  tab = (int*)malloc(size *sizeof(int));
-  for(int i = 0; i < size; i++){
-    tab[i] = i + 1;
+int** generate_combinaisons(int n) {
+  // size of the current combinaison
+  int s = 0;
+  // number of current combinaison
+  int num = 0;
+  // 1 << n == pow(2,n)
+  int** combinaisons = (int**) malloc((1 << n) * sizeof(int*));
+  for(int i = 0; i < (1 << n) ; i++){
+    //For more ease in futur with put -1 at the end of combinaison
+    combinaisons[i] = (int*) calloc(n,sizeof(int));
   }
-
-  int actual_size = 1;
-  int* combinaison = NULL;
-  /* Generate all the combinaison */
-  for (int i = 0; i < size; i++) {
-    combinaison = (int*) malloc(actual_size * sizeof(int));
-    for (int j = i; j < size; j++) {
-      combinaison[actual_size - 1] = tab[j];
-      print_tab(combinaison,actual_size);
-      actual_size++;
+  // Max we will have 2^n possibilities
+  for(int i = 1 ;i < pow(2,n);i++) {
+    // Max size of the combinaisons will be n
+    for(int j = 0; j < n;j++){
+      // We represent j in binairy and we check if we match with i
+      if ((1<<j) & i){
+        combinaisons[num][s] = j;
+        s++;
+      }
     }
-    free(combinaison);
-    actual_size = 1;
+    combinaisons[num][s] = -1;
+    num++;
+    // set or reset size of combinaison
+    s = 0;
   }
-
-  return NULL;
+  return combinaisons;
 }
--- utils.h
+++ utils.h
@@ -1,1 +1,15 @@
-int* generate_combinaisons(const int size);
+#ifndef __UTILS_H__
+#define __UTILS_H__
+
+/*
+* Generate all the combinaisons for pow(2,n) values without
+* repetitions
+*/
+int** generate_combinaisons(int n);
+
+/*
+* Print the combinaison
+*/
+void print_tab(int** tab, int n);
+
+#endif
