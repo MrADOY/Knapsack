b120cda2d25001bca0933e562b02f1238b8c1d27
Sadgasmification <jourdain.nicolas1997@gmail.com>
2018-04-24 09:39:21 +0200

    Main.c V2.1

--- main.c
+++ main.c
@@ -4,11 +4,13 @@
 #include <stdlib.h>
 #include "data.h"
 #include "display.h"
+#include "utils.h"
 
 int main(int argc, char const *argv[]) {
   printf("Le fichier %s a été compilé le %s à %s\n", __FILE__, __DATE__, __TIME__);
   int sortie=0, n=0;
   char menu1;
+  int **tab = NULL;
   if (argc>1 && argv[1])
   {
     n = atof(argv[1]);
@@ -31,13 +33,18 @@ int main(int argc, char const *argv[]) {
       case '1':
       puts("=== Variables ===");
       printf("Nombre d'objets : %d\n", n);
-
+      if (tab != NULL) {
+        print_tab(tab,n);
+      }
+      
+      puts("=== Fin de l'affichage ===");
 
       break;
 
       case '2':
       puts("=== Génération ===");
-      generate_object_randomly(0,100,n);
+      tab = generate_combinaisons(n);
+      puts("=== Nombres générés ===");
       break;
 
       case '0':
