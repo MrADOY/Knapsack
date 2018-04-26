6f68cb7c5ddffe6684bd7eca56207ec274b334c1
Aurelien Pietrzak <pietrzak.aurelien@gmail.com>
2018-04-26 09:10:14 +0200

    Tri fusion

--- utils.c
+++ utils.c
@@ -19,6 +19,61 @@ void print_tab(int** tab, int n){
 }
 
 /*
+* Sort with tri fusion
+*/
+
+void tri_fusion(int* tab,int n){
+    tri_fusion2(tab,0,n - 1);
+}
+
+void tri_fusion2(int* tab,int p, int r){
+    int quotient;
+
+    if(p < r){
+      quotient = (p + r) / 2;
+      tri_fusion2(tab,p,quotient);
+      tri_fusion2(tab,quotient + 1 ,r);
+      fusionner(tab,p,quotient,r);
+    }
+}
+
+void fusionner(int* tab, int p , int q , int r){
+  int n1 , n2;
+
+  n1 = q - p + 1;
+  n2 = r - q;
+
+  int Left[n1];
+  int Right[n2];
+
+  for (int i = 0; i < n1; i++) {
+    Left[i] = tab[p + i];
+  }
+
+  for (int j = 0; j < n2; j++) {
+    Right[j] = tab[q + j + 1];
+  }
+
+  Left[n1] = MAX + 1;
+  Right[n2] = MAX + 1;
+
+  int i = 0 ;
+  int j = 0 ;
+
+  for(int k = p ; k <= r; k++){
+    if(Left[i] <= Right[j]){
+      tab[k] = Left[i];
+      i++;
+    }else{
+      tab[k] = Right[j];
+      j++;
+    }
+
+  }
+
+}
+
+/*
 * Generate all the combinaison of n elements
 */
 
